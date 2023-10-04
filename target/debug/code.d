2a43af0649f576f4f70b923c76af7759dd2b3029
PeterTP <0petertp@gmail.com>
2023-10-04 22:06:27 +0100

    init build

---
+++ target/debug/code.d
@@ -0,0 +1,1 @@
+d:\Github\Canvord-Canvas-Discord-Bot\target\debug\code.exe: d:\Github\Canvord-Canvas-Discord-Bot\src\main.rs
---
+++ target/debug/deps/code.d
@@ -0,0 +1,5 @@
+d:\Github\Canvord-Canvas-Discord-Bot\target\debug\deps\code.exe: src\main.rs
+
+d:\Github\Canvord-Canvas-Discord-Bot\target\debug\deps\code.d: src\main.rs
+
+src\main.rs:
