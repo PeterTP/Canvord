061e2d24b2097c9faae9afda266fcd8bf9b17426
JNicoG <113068462+JNicoG@users.noreply.github.com>
2023-10-08 02:07:09 +0100

    Add dotenv, add basic bot command

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,56 @@
 use std::collections::HashMap;
 use std::io::Read;
-use std::fs;
+use std::{fs, path};
 use serde_json::Value;
+use std::env;
+use dotenv;
+
+use serenity::async_trait;
+use serenity::model::channel::Message;
+use serenity::model::gateway::Ready;
+use serenity::prelude::*;
+
+// const PING_MESSAGE: &str = "Pong!";
+// const PING_COMMAND: &str = "!ping";
+
+struct Handler;
+
+#[async_trait]
+impl EventHandler for Handler {
+    async fn message(&self, ctx: Context, msg: Message) {
+        if msg.content == "!ping" /* == PING_COMMAND */ {
+            if let Err(why) = msg.channel_id.say(&ctx.http, "Pong!").await {
+                println!("Error sending message: {:?}", why);
+            }
+        }
+    }
+
+    async fn ready(&self, _: Context, ready: Ready) {
+        println!("{} is connected!", ready.user.name);
+    }
+}
 
 // https://www.youtube.com/watch?v=dYVJQ-KQpdc REQWEST
 // https://www.youtube.com/watch?v=JYnwbczRAfo SERENITY (DISCORD) 
 
-fn main()  {
+#[tokio::main]
+async fn main() {
+    dotenv::dotenv().ok();
+    env::set_var("DISCORD_TOKEN",dotenv::var("DISCORD_TOKEN").unwrap());
+    let token = env::var("DISCORD_TOKEN").expect("Expected a token in the environment");
+    // Set gateway intents, which decides what events the bot will be notified about
+    let intents = GatewayIntents::GUILD_MESSAGES
+        | GatewayIntents::DIRECT_MESSAGES
+        | GatewayIntents::MESSAGE_CONTENT;
+
+    let mut client = Client::builder(&token, intents).event_handler(Handler).await.expect("Error creating client");
+
+    if let Err(why) = client.start().await {
+        println!("Client error: {:?}", why);
+    }
+}
+
+fn main2()  {
     canvas_get_test().unwrap();
 }
 
